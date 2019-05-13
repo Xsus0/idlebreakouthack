e2eb155287cf3aa4c950839fa841b3e9317290fd
Xsus0 <34779215+Xsus0@users.noreply.github.com>
2019-05-13 07:36:47 -0500

    Save Hack
    added full save hack

---
+++ savehack.py
@@ -0,0 +1,32 @@
+import base64
+
+print("Idle Breakout Save Hack by Xsus")
+
+# TODO prestige modification
+
+print("What level do you want to be on?")
+level = input()
+
+print("What amount of money would you like to have")
+money = input()
+
+print("How much gold do you want")
+gold = input()
+
+print("How many Black Boxes?")
+bb = input()
+
+print("How many skillpoints")
+sp = input()
+
+s = f"{level},{money},{gold},0,0,0,0,0,0,0,0,1,1,0,43595.78,999999,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,{bb},0,0,0,1,{sp},1,0,0"
+b = s.encode("UTF-8")
+e = base64.b64encode(b)
+print("Encoding....")
+print("Your result is....")
+print(e)
+print("\nCopy whats INSIDE the quotes\n")
+end = 1
+while end == 1:
+    print("Once copied you may exit the console")
+    input()
